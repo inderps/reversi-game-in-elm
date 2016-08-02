51ade2f1f0feda010a2176aacec4a0f4e003dbda
Inderpal Singh <singhinther@gmail.com>
2016-08-03 01:52:33 +0530

    source link

--- src/View.elm
+++ src/View.elm
@@ -1,7 +1,7 @@
 module View exposing (view)
 
 import Html exposing (..)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (class, href)
 import Views.Board exposing (board)
 import Views.ScoreCard exposing (scoreCard)
 import Model exposing (Model)
@@ -11,4 +11,9 @@ import Update exposing (Msg(..))
 view : Model -> Html Msg
 view model =
     div [ class "game" ]
-        [ (scoreCard model), (board model) ]
+        [ (scoreCard model)
+        , (board model)
+        , div [ class "source-link" ]
+            [ a [ href "https://github.com/inderps/reversi-game-in-elm" ] [ text "source" ]
+            ]
+        ]

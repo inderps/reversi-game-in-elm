cc4cfe4761eb0b0d81f8dee0b91cbdba24b64341
Inderpal Singh <singhinther@gmail.com>
2016-08-06 12:12:04 +0530

    added diagonal coordinates

--- src/Computations/DiscSwapping/Coordinates.elm
+++ src/Computations/DiscSwapping/Coordinates.elm
@@ -3,6 +3,7 @@ module Computations.DiscSwapping.Coordinates exposing (coordinates)
 import List exposing (map, reverse)
 import Models.Position exposing (Position, setYX, setXY)
 import Models.BoardSpecs exposing (BoardSpecs)
+import Computations.DiscSwapping.Position exposing (diagonalPositions)
 
 
 coordinates : Position -> BoardSpecs -> List (List Position)
@@ -11,6 +12,10 @@ coordinates position boardSpecs =
     , bottomCoordinates position boardSpecs
     , leftCoordinates position
     , topCoordinates position
+    , topLeftDiagonalCoordinates position
+    , bottomLeftDiagonalCoordinates position boardSpecs
+    , topRightDiagonalCoordinates position boardSpecs
+    , bottomRightDiagonalCoordinates position boardSpecs
     ]
 
 
@@ -32,3 +37,43 @@ topCoordinates position =
 bottomCoordinates : Position -> BoardSpecs -> List Position
 bottomCoordinates position boardSpecs =
     map (setXY position.x) [position.y + 1..boardSpecs.rows]
+
+
+topLeftDiagonalCoordinates : Position -> List Position
+topLeftDiagonalCoordinates position =
+    diagonalPositions position 1 1 nextTopLeftDiagonalCoordinate
+
+
+bottomLeftDiagonalCoordinates : Position -> BoardSpecs -> List Position
+bottomLeftDiagonalCoordinates position boardSpecs =
+    diagonalPositions position 1 boardSpecs.rows nextBottomLeftDiagonalCoordinate
+
+
+topRightDiagonalCoordinates : Position -> BoardSpecs -> List Position
+topRightDiagonalCoordinates position boardSpecs =
+    diagonalPositions position boardSpecs.columns 1 nextTopRightDiagonalCoordinate
+
+
+bottomRightDiagonalCoordinates : Position -> BoardSpecs -> List Position
+bottomRightDiagonalCoordinates position boardSpecs =
+    diagonalPositions position boardSpecs.columns boardSpecs.rows nextBottomRightDiagonalCoordinate
+
+
+nextTopLeftDiagonalCoordinate : Position -> Position
+nextTopLeftDiagonalCoordinate position =
+    setXY (position.x - 1) (position.y - 1)
+
+
+nextBottomLeftDiagonalCoordinate : Position -> Position
+nextBottomLeftDiagonalCoordinate position =
+    setXY (position.x - 1) (position.y + 1)
+
+
+nextTopRightDiagonalCoordinate : Position -> Position
+nextTopRightDiagonalCoordinate position =
+    setXY (position.x + 1) (position.y - 1)
+
+
+nextBottomRightDiagonalCoordinate : Position -> Position
+nextBottomRightDiagonalCoordinate position =
+    setXY (position.x + 1) (position.y + 1)
--- src/Computations/DiscSwapping/Position.elm
+++ src/Computations/DiscSwapping/Position.elm
@@ -1,9 +1,15 @@
-module Computations.DiscSwapping.Position exposing (occupiedTilesInTheseCoordinatesList, isSameCoordinates)
+module Computations.DiscSwapping.Position
+    exposing
+        ( occupiedTilesInTheseCoordinatesList
+        , isSameCoordinates
+        , diagonalPositions
+        )
 
 import List exposing (map, member)
-import List.Extra exposing (find, takeWhile)
+import List.Extra exposing (find, takeWhile, last)
 import Models.OccupiedTile exposing (OccupiedTile)
-import Models.Position exposing (Position)
+import Models.Position exposing (Position, setXY)
+import Models.BoardSpecs exposing (BoardSpecs)
 import Extensions.List exposing (maybeListToListUntil)
 
 
@@ -26,3 +32,35 @@ ifOccupied occupiedTiles position =
 isSameCoordinates : Position -> OccupiedTile -> Bool
 isSameCoordinates position tile =
     position.x == tile.position.x && position.y == tile.position.y
+
+
+diagonalPositions : Position -> Int -> Int -> (Position -> Position) -> List Position
+diagonalPositions position x y nextPosition =
+    if position.x == x || position.y == y then
+        []
+    else
+        diagonalPositions' x y nextPosition [ nextPosition position ]
+
+
+diagonalPositions' : Int -> Int -> (Position -> Position) -> List Position -> List Position
+diagonalPositions' x y nextPosition positions =
+    case (last positions) of
+        Just position ->
+            if position.x == x || position.y == y then
+                positions
+            else
+                appendedPositionsUsing nextPosition positions
+                    |> diagonalPositions' x y nextPosition
+
+        Nothing ->
+            positions
+
+
+appendedPositionsUsing : (Position -> Position) -> List Position -> List Position
+appendedPositionsUsing nextPosition positions =
+    case (last positions) of
+        Just position ->
+            positions ++ [ nextPosition position ]
+
+        Nothing ->
+            positions
